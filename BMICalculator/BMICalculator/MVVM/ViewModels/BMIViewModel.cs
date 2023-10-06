755797e6ab85d71240d57b2c3028ade5b2e86613
Brett Lazarine <jblazarine@gmail.com>
2023-10-06 01:53:33 -0500

    complete project

---
+++ BMICalculator/BMICalculator/MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BMICalculator.MVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator.MVVM.ViewModels
+{
+    public class BMIViewModel
+    {
+        public BMI BMI { get; set; }
+
+        public BMIViewModel()
+        {
+            BMI = new BMI()
+            {
+                Height = 180,
+                Weight = 73
+            };
+        }
+    }
+}
---
+++ BMICalculator/BMICalculator/MVVM/Views/BMIView.xaml.cs
@@ -0,0 +1,13 @@
+using BMICalculator.MVVM.ViewModels;
+
+namespace BMICalculator.MVVM.Views;
+
+public partial class BMIView : ContentPage
+{
+	public BMIView()
+	{
+		InitializeComponent();
+
+		BindingContext = new BMIViewModel();
+	}
+}
